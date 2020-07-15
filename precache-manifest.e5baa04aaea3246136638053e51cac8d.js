30021ac2c81b6e32d69d254bfc7147d95770aa42
Tori Miller <millertori04@gmail.com>
2020-07-14 21:53:23 -0700

    Updates

---
+++ precache-manifest.e5baa04aaea3246136638053e51cac8d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b459bd1ebfde490d5394d4ec60eebd",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "5362869387c340790e67",
+    "url": "/deploy-react/static/css/main.17156fa4.chunk.css"
+  },
+  {
+    "revision": "efba445723594d0a74f9",
+    "url": "/deploy-react/static/js/2.f0796eee.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/deploy-react/static/js/2.f0796eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5362869387c340790e67",
+    "url": "/deploy-react/static/js/main.128a223a.chunk.js"
+  },
+  {
+    "revision": "f1eb0ed5ee057ec89de1",
+    "url": "/deploy-react/static/js/runtime-main.08a5dfb5.js"
+  },
+  {
+    "revision": "847be94530703b50af2f6b268deccb3f",
+    "url": "/deploy-react/static/media/autolog.847be945.png"
+  },
+  {
+    "revision": "3e0791163da9554d5a7ced24edef1a8c",
+    "url": "/deploy-react/static/media/puppypal.3e079116.png"
+  },
+  {
+    "revision": "66c012bb707d5e4aa4c5205d915642c9",
+    "url": "/deploy-react/static/media/twitter.66c012bb.png"
+  }
+]);
